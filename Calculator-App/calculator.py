1a6be611f69eb5cb0776600cff89bc33d289fba5
uppara saroja <264395373+upparasaroja-coder@users.noreply.github.com>
2026-09-16 21:09:38 +0530

    Add files via upload

---
+++ Calculator-App/calculator.py
@@ -0,0 +1,27 @@
+print("===== Simple Calculator =====")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print("Result:", result)
+
+elif operator == "-":
+    result = num1 - num2
+    print("Result:", result)
+
+elif operator == "*":
+    result = num1 * num2
+    print("Result:", result)
+
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Cannot divide by zero.")
+
+else:
+    print("Invalid operator.")
